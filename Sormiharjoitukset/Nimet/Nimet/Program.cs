71254550a5be8dfb995759a8b70bcb15fd0d4ef3
Niia <Niia@PC>
2017-01-11 03:12:38 +0200

    Initial commit.

---
+++ Sormiharjoitukset/Nimet/Nimet/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+/* Sormiharjoitus: Käyttäjien nimet aakkosjärjestykseen
+ * Tee ohjelma joka kysyy käyttäjältä viisi (5) etunimeä. Tämän jälkeen ohjelma näyttää nimet annetussa järjestyksessä sekä aakkosjärjestyksessä. */
+
+namespace Sormiharjoitus1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Kysytään nimet.
+            string[] names = new string[5];
+            Console.WriteLine("Anna käyttäjien etunimet: ");
+            for (int i = 0; i < names.Length; i++)
+            {
+                names[i] = Console.ReadLine();
+            }
+            // Näytetään nimet annetussa järjestyksessä.
+            Console.WriteLine("\nAnnoit nimet: ");
+            for (int i = 0; i < names.Length; i++)
+            {
+                Console.WriteLine(names[i]);
+            }
+            // Järjestetään ja näytetään nimet aakkosjärjestyksessä.
+            Console.WriteLine("\nNimet aakkosjärjestyksessä: ");
+            Array.Sort(names);
+            for (int i = 0; i < names.Length; i++)
+            {
+                Console.WriteLine(names[i]);
+            }
+        }
+    }
+}
