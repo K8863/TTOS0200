fed790be6e50cdc92962148f3c69d1ef528d7654
Niia <Niia@PC>
2017-01-11 05:07:59 +0200

    Initial commit.

---
+++ Sormiharjoitukset/Lotto/Program.cs
@@ -0,0 +1,63 @@
+﻿/* Sormiharjoitus: Lotto 7/40.
+ * Suomalaisessa Lotossa arvotaan seitsemän (7) numeroa 40:sta. 
+ * Tee ohjelma, joka arpoo käyttäjän haluaman määrän Lotto-rivejä. */
+
+using System;
+using System.Linq;
+
+namespace Lotto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Lottonumeroiden muuttujien luonti
+            const int a = 7;
+            int[] numerot = new int[40];
+            int[] voittonumerot = new int[7];
+            int rand;
+            Random rng = new Random();
+
+            // Kysytään haluttujen rivien määrä
+            Console.Write("Anna arvottavien rivien määrä > ");
+            int rivit = int.Parse(Console.ReadLine());
+            int b = 0;
+
+            while (b < rivit)
+            {
+                // Numerot taulukkoon
+                for (int i = 0; i < numerot.Length; i++)
+                {
+                    numerot[i] = i;
+                }
+                // Arvotaan numerot
+                for (int i = 0; i < a; i++)
+                {
+                    // Asetetaan randomoitu luku rand-muuttujaan
+                    rand = rng.Next(numerot[39]);
+                    // Tarkistetaan onko luku jo voittonumeroissa
+                    if (voittonumerot.Contains(rand))
+                    {
+                        i--;
+                    }
+                    // Jos ei, niin tallennetaan se voittonumeroihin
+                    else
+                    {
+                        voittonumerot[i] = rand;
+                    }
+                }
+                // Laitetaan numerot suuruusjärjestykseen
+                Array.Sort(voittonumerot);
+
+                // Tulostetaan numerot
+                Console.Write("Rivi " + (b + 1) + ": ");
+                for (int i = 0; i < a; i++)
+                {
+                    Console.Write(voittonumerot[i] + " ");
+                }
+                Console.Write("\n");
+                b++;
+            }
+        }
+    }
+}
