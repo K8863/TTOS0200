10c64f16ee636878a1247029f4809e377ffcdf5d
Niia <Niia@PC>
2017-01-17 10:03:41 +0200

    Initial commit.

---
+++ Labra 03/T04/Program.cs
@@ -0,0 +1,43 @@
+﻿/* Kurssi: TTOS0200 Olio-ohjelmointi
+ * Tehtävä: Labra 03 - Tehtävä 4
+ * Tekijä: Niia Neuvonen K8863
+ * Pvm: 17.1.2017
+ * Kuvaus: 
+ *          Luo Vehicle-luokka seuraavien tietojen mukaisesti:
+ *          
+ *          ominaisuudet
+ *              Name:string
+ *              Speed:int
+ *              Tyres:int
+ *          
+ *          toiminnot
+ *              PrintData(), tulostaa Vehiclen ominaisuudet näytölle
+ *              ToString():string, palauttaa kaikki Vehiclen ominaisuudet yhtenä merkkijonona
+ *              
+ *          Toteuta Vehicle-luokan ohjelmointi sekä pääohjelma, jolla luot olion Vehicle-luokasta. 
+ *          Muuta olion arvoja ja tulosta olion arvoja konsolille. */
+
+using System;
+using JAMK.IT;
+
+namespace T04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Luodaan vehicle-olio
+            Vehicle vehicle = new JAMK.IT.Vehicle();
+            // Annetaan oliolle arvot
+            vehicle.Name = "Kaara";
+            vehicle.Speed = 180;
+            vehicle.Tyres = 20;
+            // Tulostetaan ominaisuudet
+            vehicle.PrintData();
+            // Tulostetaan ominaisuudet yhtenä merkkijonona
+            string merkkijono;
+            merkkijono = vehicle.ToString();
+            Console.WriteLine("\n" + merkkijono + "\n");
+        }
+    }
+}
---
+++ Labra 03/T04/Vehicle.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JAMK.IT
+{
+    public class Vehicle
+    {
+        public string Name;
+        public int Speed;
+        public int Tyres;
+
+        // Tulostaa auton ominaisuudet
+        public void PrintData()
+        {
+            Console.WriteLine("AUTON OMINAISUUDET\n");
+            Console.WriteLine("Auto: {0}", Name);
+            Console.WriteLine("Auton nopeus: {0} km/h", Speed);
+            Console.WriteLine("Auton renkaat: {0} tuumaa", Tyres);
+        }
+
+        // Palauttaa kaikki auton ominaisuudet yhtenä merkkijonona
+        public string ToString()
+        {
+            string merkkijono;
+            merkkijono = Name + ", nopeus " + Speed + " km/h, renkaiden koko " + Tyres + " tuumaa";
+            return merkkijono;
+        }
+        
+    }
+}
