6562ecc3575141adb2d938132687e35a315e8560
K8863 <K8863@TIET-334-WS-20>
2017-01-17 16:18:58 +0200

    Initial commit.

---
+++ Labra 03/T05/Opiskelija.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JAMK.IT
+{
+    public class Opiskelija
+    {
+        public int Ika { get; set; }
+        public string Luokka { get; set; }
+        public string Nimi { get; set; }
+
+        public Opiskelija(string Name, int Age, string Class) 
+        {
+            Nimi = Name;
+            Ika = Age;
+            Luokka = Class;
+        }
+    }
+}
---
+++ Labra 03/T05/Program.cs
@@ -0,0 +1,34 @@
+﻿/* Kurssi: TTOS0200 Olio-ohjelmointi
+ * Tehtävä: Labra 03 - Tehtävä 5
+ * Tekijä: Niia Neuvonen K8863
+ * Pvm: 17.1.2017
+ * Kuvaus: 
+ *          Suunnittele UML-editorilla Opiskelija-luokka, joka sisältää opiskelijalle tyypillisiä tietoja ja toimintoja. 
+ *          Tee uusi projekti. Ohjelmoi Opiskelija-luokka sekä toteuta pääohjelma, 
+ *          joka luo muutamia opiskelijoita ja tallentaa opiskelijat taulukkoon (esim. 5 opiskelijaa). 
+ *          Tulosta taulukossa olevien opiskelijoiden tiedot käyttämällä toistorakennetta. */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using JAMK.IT;
+
+namespace T05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            OpiskelijaHallinta();
+        }
+
+        static void OpiskelijaHallinta()
+        {
+            Opiskelija a = new JAMK.IT.Opiskelija("Matti", 25, "K8912");
+            Opiskelija b = new JAMK.IT.Opiskelija("Mauno", 21, "K9771");
+            Opiskelija c = new JAMK.IT.Opiskelija("Jaana", 23, "K9155");
+        }
+    }
+}
